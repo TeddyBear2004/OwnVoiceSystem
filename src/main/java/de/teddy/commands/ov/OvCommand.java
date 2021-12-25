70d4fd7a60e97014edcfaa4979c585d6eb54ef1d
TeddyBear2004 <thorgeengelmann@gmail.com>
2021-12-25 22:55:55 +0100

    added ov command

--- src/main/java/de/teddy/commands/ov/OvCommand.java
+++ src/main/java/de/teddy/commands/ov/OvCommand.java
@@ -13,79 +13,89 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import reactor.core.publisher.Mono;
 
-import java.util.function.Consumer;
-
 public class OvCommand implements CommandExecutable {
-    private static Consumer<EmbedCreateSpec> OV_COMMANDS = null;
-    private static Consumer<EmbedCreateSpec> OV_COMMANDS_FOR_ADMINS = null;
+    private static final EmbedCreateSpec OV_COMMANDS;
+    private static final EmbedCreateSpec OV_COMMANDS_FOR_ADMINS;
 
     static{
-        /*OV_COMMANDS = embedCreateSpec ->
-                embedCreateSpec.setTitle("Possible commands:")
-                        .addField("!ov addAdmin [@User#1234], [@User2#1234]", "Gibt einem Nutzer das Recht, den Talk zu editieren.", false)
-                        .addField("!ov removeAdmin [@User#1234], [@User2#1234]", "Entzieht einem Nutzer das Recht, den Talk zu editieren.", false)
+        OV_COMMANDS = EmbedCreateSpec.builder()
+                .title("Possible commands:")
+                .addField("!ov addAdmin [@User#1234], [@User2#1234]", "Gibt einem Nutzer das Recht, den Talk zu editieren.", false)
+                .addField("!ov removeAdmin [@User#1234], [@User2#1234]", "Entzieht einem Nutzer das Recht, den Talk zu editieren.", false)
 
-                        .addField("!ov add [@User#1234], [@User2#1234]", "Das Gegenteil von !ov remove. Erlaubt den angegebenen Usern die Sichtbarkeit des Kanals, sowie sich zu Verbinden.", false)
-                        .addField("!ov remove [@User#1234], [@User2#1234]", "Verbietet den angegegebenen Usern die Sichtbarkeit des Kanals, sowie sich zu Verbinden.", false)
+                .addField("!ov add [@User#1234], [@User2#1234]", "Das Gegenteil von !ov remove. Erlaubt den angegebenen Usern die Sichtbarkeit des Kanals, sowie sich zu Verbinden.", false)
+                .addField("!ov remove [@User#1234], [@User2#1234]", "Verbietet den angegegebenen Usern die Sichtbarkeit des Kanals, sowie sich zu Verbinden.", false)
 
-                        .addField("!ov kick [@User#1234], [@User2#1234]", "Trennt die Verbindung von den angegegebenen Usern.", false)
+                .addField("!ov kick [@User#1234], [@User2#1234]", "Trennt die Verbindung von den angegegebenen Usern.", false)
 
-                        .addField("!ov show", "Erlaubt allen Nutzern das sehen des Sprachkanals.", false)
-                        .addField("!ov hide", "Verbietet allen Nutzern das sehen des Sprachkanals.", false)
+                .addField("!ov show", "Erlaubt allen Nutzern das sehen des Sprachkanals.", false)
+                .addField("!ov hide", "Verbietet allen Nutzern das sehen des Sprachkanals.", false)
 
-                        .addField("!ov unlock", "Erlaubt allen Nutzern das Verbinden mit dem Sprachkanal", false)
-                        .addField("!ov lock", "Verbietent allen Nutzern das Verbinden mit dem Sprachkanal", false)
+                .addField("!ov unlock", "Erlaubt allen Nutzern das Verbinden mit dem Sprachkanal", false)
+                .addField("!ov lock", "Verbietent allen Nutzern das Verbinden mit dem Sprachkanal", false)
 
-                        .addField("!ov bitrate [8-96]", "Ändert die Bitrate des Sprachkanals.", false)
+                .addField("!ov bitrate [8-96]", "Ändert die Bitrate des Sprachkanals.", false)
 
-                        .addField("!ov rename [Name]", "Setzt den Namen des Voicechannels.", false)
+                .addField("!ov rename [Name]", "Setzt den Namen des Voicechannels.", false)
 
-                        .addField("!ov limit [0-99]", "Setzt die maximale Nutzeranzahl für den Talk. 0 für unbegrenzte Nutzer.", false)
+                .addField("!ov limit [0-99]", "Setzt die maximale Nutzeranzahl für den Talk. 0 für unbegrenzte Nutzer.", false)
 
-                        .addField("!ov stayAlive [hours]", "Setzt die Zeit, die der Talk am Leben bleibt ohne Nutzer.", false)
+                .addField("!ov stayAlive [hours]", "Setzt die Zeit, die der Talk am Leben bleibt ohne Nutzer.", false)
 
-                        .addField("!ov save", "Speichert die aktuelle Configuration. Diese wird mit !ov load wieder geladen oder durch das neu erstellen des Talks.", false)
-                        .addField("!ov load", "Lädt die mit !ov save gespeicherte Konfiguration.", false);
+                .addField("!ov save", "Speichert die aktuelle Configuration. Diese wird mit !ov load wieder geladen oder durch das neu erstellen des Talks.", false)
+                .addField("!ov load", "Lädt die mit !ov save gespeicherte Konfiguration.", false)
+                .build();
 
 
-        OV_COMMANDS_FOR_ADMINS = embedCreateSpec ->
-                embedCreateSpec
-                        .setTitle("Possible commands:")
-                        .addField("!ov addAdmin [@User#1234], [@User2#1234]", "Gibt einem Nutzer das Recht, den Talk zu editieren.", false)
-                        .addField("!ov removeAdmin [@User#1234], [@User2#1234]", "Entzieht einem Nutzer das Recht, den Talk zu editieren.", false)
+        OV_COMMANDS_FOR_ADMINS = EmbedCreateSpec.builder()
+                .title("Possible commands:")
+                .addField("!ov addAdmin [@User#1234], [@User2#1234]", "Gibt einem Nutzer das Recht, den Talk zu editieren.", false)
+                .addField("!ov removeAdmin [@User#1234], [@User2#1234]", "Entzieht einem Nutzer das Recht, den Talk zu editieren.", false)
 
-                        .addField("!ov add [@User#1234], [@User2#1234]", "Das Gegenteil von !ov remove. Erlaubt den angegebenen Usern die Sichtbarkeit des Kanals, sowie sich zu Verbinden.", false)
-                        .addField("!ov remove [@User#1234], [@User2#1234]", "Verbietet den angegegebenen Usern die Sichtbarkeit des Kanals, sowie sich zu Verbinden.", false)
+                .addField("!ov add [@User#1234], [@User2#1234]", "Das Gegenteil von !ov remove. Erlaubt den angegebenen Usern die Sichtbarkeit des Kanals, sowie sich zu Verbinden.", false)
+                .addField("!ov remove [@User#1234], [@User2#1234]", "Verbietet den angegegebenen Usern die Sichtbarkeit des Kanals, sowie sich zu Verbinden.", false)
 
-                        .addField("!ov kick [@User#1234], [@User2#1234]", "Trennt die Verbindung von den angegegebenen Usern.", false)
+                .addField("!ov kick [@User#1234], [@User2#1234]", "Trennt die Verbindung von den angegegebenen Usern.", false)
 
-                        .addField("!ov show", "Erlaubt allen Nutzern das sehen des Sprachkanals.", false)
-                        .addField("!ov hide", "Verbietet allen Nutzern das sehen des Sprachkanals.", false)
+                .addField("!ov show", "Erlaubt allen Nutzern das sehen des Sprachkanals.", false)
+                .addField("!ov hide", "Verbietet allen Nutzern das sehen des Sprachkanals.", false)
 
-                        .addField("!ov unlock", "Erlaubt allen Nutzern das Verbinden mit dem Sprachkanal", false)
-                        .addField("!ov lock", "Verbietent allen Nutzern das Verbinden mit dem Sprachkanal", false)
+                .addField("!ov unlock", "Erlaubt allen Nutzern das Verbinden mit dem Sprachkanal", false)
+                .addField("!ov lock", "Verbietent allen Nutzern das Verbinden mit dem Sprachkanal", false)
 
-                        .addField("!ov bitrate [8-96]", "Ändert die Bitrate des Sprachkanals.", false)
+                .addField("!ov bitrate [8-96]", "Ändert die Bitrate des Sprachkanals.", false)
 
-                        .addField("!ov rename [Name]", "Setzt den Namen des Voicechannels.", false)
+                .addField("!ov rename [Name]", "Setzt den Namen des Voicechannels.", false)
 
-                        .addField("!ov limit [0-99]", "Setzt die maximale Nutzeranzahl für den Talk. 0 für unbegrenzte Nutzer.", false)
+                .addField("!ov limit [0-99]", "Setzt die maximale Nutzeranzahl für den Talk. 0 für unbegrenzte Nutzer.", false)
 
-                        .addField("!ov stayAlive [hours]", "Setzt die Zeit, die der Talk am Leben bleibt ohne Nutzer.", false)
+                .addField("!ov stayAlive [hours]", "Setzt die Zeit, die der Talk am Leben bleibt ohne Nutzer.", false)
 
-                        .addField("!ov save", "Speichert die aktuelle Configuration. Diese wird mit !ov load wieder geladen oder durch das neu erstellen des Talks.", false)
-                        .addField("!ov load", "Lädt die mit !ov save gespeicherte Konfiguration.", false)
+                .addField("!ov save", "Speichert die aktuelle Configuration. Diese wird mit !ov load wieder geladen oder durch das neu erstellen des Talks.", false)
+                .addField("!ov load", "Lädt die mit !ov save gespeicherte Konfiguration.", false)
 
-                        .addField("** **", "** **", false)
-                        .addField("*FÜR ADMINS*", "** **", false)
-                        .addField("!ov createSystem", "Erstellt ein OV-System auf diesem Server", false)
-                        .addField("!ov deleteSystem [ID from the channel or category]", "Löscht das OV-System auf diesem Server", false)
+                .addField("** **", "** **", false)
+                .addField("*FÜR ADMINS*", "** **", false)
+                .addField("!ov createSystem", "Erstellt ein OV-System auf diesem Server", false)
+                .addField("!ov deleteSystem [ID from the channel or category]", "Löscht das OV-System auf diesem Server", false)
 
-                        .addField("!ov setDefaultRole [@Role]", "The role highest role, everyone have.", false);/**/
+                .addField("!ov setDefaultRole [@Role]", "The role highest role, everyone have.", false)
+                .build();
     }
 
     @Override
-    public Mono<Message> execute(@NotNull String[] strings, String[] strings1, @NotNull User user, @Nullable Command command, @NotNull MessageChannel messageChannel, @NotNull GatewayDiscordClient gatewayDiscordClient){
-        return null;
+    public @NotNull Mono<Message> execute(@NotNull String[] strings, String[] strings1, @NotNull User user, @Nullable Command command, @NotNull MessageChannel messageChannel, @NotNull GatewayDiscordClient gatewayDiscordClient){
+        if(user instanceof Member){
+            Member member = (Member)user;
+            return member.getBasePermissions()
+                    .flatMap(permissions -> {
+                        if(permissions.contains(Permission.ADMINISTRATOR)){
+                            return messageChannel.createMessage(OV_COMMANDS_FOR_ADMINS);
+                        }else{
+                            return messageChannel.createMessage(OV_COMMANDS);
+                        }
+                    });
+        }
+        return messageChannel.createMessage(OV_COMMANDS_FOR_ADMINS);
     }
 }
